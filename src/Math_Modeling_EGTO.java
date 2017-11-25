690a4e39e2d206ad41121843114b0b389c6cab12
Yegor <Voute@users.noreply.github.com>
2017-11-25 04:07:17 +0400

    practice 1, task 1.1, 1.2, 1.3

--- src/Math_Modeling_EGTO.java
+++ src/Math_Modeling_EGTO.java
@@ -3,5 +3,6 @@ public class Math_Modeling_EGTO
     public static void main(String ... args)
     {
         Practice_1.task_1();
+//        Practice_1.task_1n();
     }
 }
--- src/Practice_1.java
+++ src/Practice_1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practice_1
 {
 //    Используя метод вычетов, сгенерировать последовательность из 1 000 псевдослучайных чисел, результат вывести на экран.
@@ -8,19 +11,63 @@ public class Practice_1
     public static void task_1()
     {
         int size = 1000;
-        float[] x = new float[size];
-        x[0] = (float)(Math.pow(2d, -23d));
-        int M = 637456296;
+        double M = 7d;
+        double m = 7d;
+        double v = 1d / (double)size;
+        double Mo = 0;
+        double D = 0;
+        Map<Double, Integer> frequency = new HashMap<Double, Integer>();
+
+        double[] x = new double[size];
+        x[0] = Math.pow(2, -m);
+        Mo = increaseMo(Mo, x[0], v);
+        D = increaseD(D, x[0], v);
+        countFreq(frequency, x[0]);
+        printNumber(String.valueOf(x[0]), 1);
 
         for (int i = 1; i < size; i++)
         {
-            x[i] = M * x[ - 1];
-            printNumber(String.valueOf(x[i]), i);
+            double newNum = M * x[i - 1];
+            x[i] = newNum - (long)newNum;
+            Mo = increaseMo(Mo, x[i], v);
+            D = increaseD(D, x[i], v);
+            countFreq(frequency, x[i]);
+            printNumber(String.valueOf(x[i]), i + 1);
+        }
+
+        D -= Math.pow(Mo, 2);
+        System.out.println("M{X} = " + Mo);
+        System.out.println("D{X} = " + D);
+
+        for (int i=0; i < frequency.size(); i++)
+        {
+            System.out.print("key " + frequency.keySet().toArray()[i] + " ");
+            System.out.println("value " + frequency.values().toArray()[i]);
+        }
+    }
+
+    static double increaseMo(double Mo, double n, double v)
+    {
+        return Mo + n * v;
+    }
+
+    static double increaseD(double D, double n, double v)
+    {
+        return D + Math.pow(n, 2) * v;
+    }
+
+    static void countFreq(Map<Double, Integer> f, double n)
+    {
+        if (f.containsKey(n))
+        {
+            f.put(n, f.get(n).intValue() + 1);
+        } else {
+            f.put(n, 1);
         }
     }
 
     public static void printNumber(String n, int id)
     {
-        System.out.println("число №" + id + ": " + n);
+        System.out.println("number #" + id + ": " + n);
     }
 }
